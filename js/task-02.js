02b1aa4ae3bbe3740d8322f3c33172d57986e4b1
Roman Kruk <wilmlass@gmail.com>
2021-07-01 13:02:51 +0300

    complete task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ulIngrRef = document.querySelector('#ingredients');
+const ulIngrItemRef = document.createElement('li');
+
+ingredients.forEach(ingr => {
+  const ulIngrItemRef = document.createElement('li');
+  ulIngrItemRef.textContent = `${ingr}`;
+  ulIngrRef.append(ulIngrItemRef);
+});
