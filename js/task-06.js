8eba5e0c99742812cfffe54381f6acc334f023c6
Roman Kruk <wilmlass@gmail.com>
2021-07-01 19:36:48 +0300

    complete task6

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+const inputRef = document.querySelector('#validation-input');
+
+inputRef.addEventListener('blur', event => {
+  if (event.currentTarget.value.length < Number(inputRef.dataset.length)) {
+    inputRef.classList.add('invalid') ||
+      inputRef.classList.replace('valid', 'invalid');
+  } else inputRef.classList.replace('invalid', 'valid');
+});
