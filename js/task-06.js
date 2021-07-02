d8cb50ece781a90fe661ec44eb69dfd4cdf8993f
Roman Kruk <wilmlass@gmail.com>
2021-07-02 15:31:53 +0300

    corrections

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', event => {
-  if (event.currentTarget.value.length < Number(inputRef.dataset.length)) {
+  if (event.currentTarget.value.length != Number(inputRef.dataset.length)) {
     inputRef.classList.add('invalid') ||
       inputRef.classList.replace('valid', 'invalid');
   } else inputRef.classList.replace('invalid', 'valid');
